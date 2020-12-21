b7e467605175fcc946ba07af9a8fa808c9305bcb
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-21 03:10:18 -0600

    got food to work

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 #! /usr/bin/env node
 
+const axios = require('axios')
+
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-readline.question("What would you like to log today? ", item => {
-    console.log(item);
+readline.question("What would you like to log today? ", async (item) => {
+    let { data } = await axios.get('http://localhost:3001/food');
+    let it = data[Symbol.iterator]();
+    let position = it.next()
+    while (!position.done) {
+        const food = position.value.name;
+        if (food === item) {
+            console.log(`${item} has ${position.value.calories} calories`)
+        }
+        position = it.next();
+    }
     readline.close()
 })
 
